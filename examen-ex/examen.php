47c92e8aae28b85c9140a4018e715c4cc74950db
unknown <jordicalderon@gmail.com>
2020-03-11 13:09:16 +0100

    .

--- examen-ex/examen.php
+++ examen-ex/examen.php
@@ -2,16 +2,7 @@
 
 /* Solamente trabajamos si nos han enviado el parámetro "notas" desde el formulario,
     sino, volvemos al formulario */
-    
-    print "Contenido de post Enviar:".var_export($_POST["Enviar"]);
-    print "<hr>";
-    print "Contenido de is_set: ".var_export(isset($_POST["Enviar"]));
-    print "<hr>";
-    print "Contenido de isnull: ".var_export(is_null($_POST["Enviar"]));
-    print "<hr>";
-    print "Contenido de empty: ".var_export(empty($_POST["notas"]));
-    print "<hr>";
-       
+           
     if(isset($_POST["Enviar"]) && !estaVacioFormulario($_POST["notas"])){
 
         $notas=$_REQUEST["notas"];
@@ -25,11 +16,9 @@
 
             if( !is_numeric($valor) ){
                 array_push($errores, "La nota ".($key + 1)." no es numérica. Has puesto ($valor)");
-                //echo "La nota $key no es numérica</br>";
                 $hayErrores = true;
             }else {
                 if(!esRangoAdmitido(intval($valor))){
-                    //echo "La nota $key no tiene el rango permitido ($valor).</br>";
                     array_push($errores, "La nota $key no tiene el rango permitido. Has puesto ($valor) y sólo admito números del 0 al 10");
                     $hayErrores = true;
                 }
@@ -64,7 +53,7 @@
             echo "</br><a href='javascript:history.back();'>Volver atrás</a>";     
         }        
     }else {
-       // header('Location: http://localhost/matins/examen-ex/examen.html');
+       header('Location: http://localhost/matins/examen-ex/examen.html');
     }
 
     function esRangoAdmitido($valor){
@@ -72,10 +61,12 @@
     }
 
     function estaVacioFormulario($array){
-        $liRet = false;
+        $liRet = true;
 
         foreach($array as $key => $valor ){
-            
+            if (strlen($valor) > 0){
+                $liRet = false;
+            } 
         }
 
         return $liRet;
